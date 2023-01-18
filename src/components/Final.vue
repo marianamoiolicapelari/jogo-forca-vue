7b9eef96db9b1276e4218a142aeeb2eb8dddfa02
Mariana M.C <marianamoioli@gmail.com>
2023-01-18 14:53:17 -0300

    first commit

---
+++ src/components/Final.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="final">
+    <div :class="`final-texto ${etapa}`">{{ texto }}</div>
+  </div>
+  <button 
+    class="final-botão"
+    v-on:click="jogarNovamente">Jogar novamente</button>
+</template>
+
+<script>
+export default {
+  name: "Final",
+  props: {
+    etapa: String,
+    texto: String,
+    jogarNovamente: Function
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped>
+.final-texto {
+  margin: 20px 0px;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.final-texto.enforcado {
+  color: var(--color-text-error);
+}
+
+.final-texto.ganhador {
+  color: var(--color-text-success);
+}
+</style>
